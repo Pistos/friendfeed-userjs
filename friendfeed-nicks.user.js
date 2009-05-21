645178314bc898b92d7972c8dc67790a67faaca4
Pistos <gitsomegrace.5.pistos@geoshell.com>
2009-05-21 11:51:50 -0400

    Added some more header comments. Changed condition from "check for space" to "!= sid".

--- friendfeed-nicks.user.js
+++ friendfeed-nicks.user.js
@@ -9,16 +9,22 @@
 // by: Pistos
 // Licence: MIT licence
 // Copyright (c) Pistos 2009
+//
+// Version: 0.9.0
+// Last updated: 2009-05-21
 
 var ffn_jQuery;
 if( typeof unsafeWindow != 'undefined' ) {
+    // Greasemonkey
     ffn_jQuery = unsafeWindow.jQuery;
 } else {
+    // Other (e.g. Opera)
     ffn_jQuery = jQuery;
 }
 
-ffn_jQuery( 'div.name > a' ).each( function() {
-    if( ffn_jQuery(this).text().indexOf( ' ' ) > -1 ) {
+ffn_jQuery( 'div.name a.l_profile' ).each( function() {
+    var link = ffn_jQuery(this);
+    if( link.attr( 'href' ) != '/' + link.attr( 'sid' ) ) {
         ffn_jQuery(this).text( ffn_jQuery(this).attr( 'href' ).substr( 1 ) );
     }
 } );
